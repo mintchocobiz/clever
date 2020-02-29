d49a027a9bd475615187497dfe6affbdab3165fc
greatbk <greatbk@daum.net>
2020-02-29 14:19:54 +0900

    add XssType.java

--- src/main/java/biz/mintchoco/clever/config/XssConfig.java
+++ src/main/java/biz/mintchoco/clever/config/XssConfig.java
@@ -1,5 +1,6 @@
 package biz.mintchoco.clever.config;
 
+import biz.mintchoco.clever.util.wrapper.XssType;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -16,10 +17,14 @@ import java.util.List;
 @ConfigurationProperties(prefix = "xss")
 public class XssConfig {
 
+    private XssType type;
+
     private List<String> excludePaths;
 
     @PostConstruct
     public void info() {
+        log.debug("xss config -> type");
+        log.debug(type.name());
         log.debug("xss config -> exclude-path");
         excludePaths.forEach(item -> log.debug(item));
     }
---
+++ src/main/java/biz/mintchoco/clever/util/wrapper/XssType.java
@@ -0,0 +1,9 @@
+package biz.mintchoco.clever.util.wrapper;
+
+public enum XssType {
+
+    ALL,
+    HARDCORE,
+    REGEX,
+    ;
+}
